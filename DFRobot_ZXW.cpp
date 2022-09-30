15589e28b2dbfb22bb1016e24aee6f99b376a363
Sivan6 <924063564@qq.com>
2022-09-30 09:37:41 +0800

    DFRobot_ZXW_SENSOR

--- DFRobot_ZXW.cpp
+++ DFRobot_ZXW.cpp
@@ -1,51 +1,177 @@
 #include <DFRobot_ZXW.h>
-#include <string.h>
 
-DFRobot_ZXW::DFRobot_ZXW(){	//构造函数的重写
-	Serial.println("hello DFRobot!!!");
-}
-DFRobot_ZXW::~DFRobot_ZXW(){	//虚构函数的重写
-	Serial.println("byebye DFRobot");
-}
-//设置姓名函数
-int DFRobot_ZXW::setName(const char *name)
+DFRobot_ZXW::DFRobot_ZXW(uint8_t mode)
+:_mode(mode)
 {
-	if(strlen(name)<20)
-	{
-		_name=name;
-		return 0;
-	}
-	return -1;
-}
-//设置年龄函数（输入整数类型）
-int DFRobot_ZXW::setAge(uint8_t age)
-{
-	if((age>18)||(age<100))
-	{
-		_age=age;
-		return 0;
-	}
-	return -1;
-}
-//设置姓名函数（输入字符串类型）
-int DFRobot_ZXW::setAge(String age)
-{
-	int tem = age.toInt();//字符串转整形函数
-	if((tem>18)||(tem<100))
-	{
-		_age=tem;
-		return 1;
-	}
-	return 0;
-}
-//得到姓名函数
-String DFRobot_ZXW::getName(void)
-{
-	return _name;
-}
-//得到年龄函数
-uint8_t DFRobot_ZXW::getAge(void)
-{
-	return _age;
+	
 }
 
+DFRobot_ZXW_IIC::DFRobot_ZXW_IIC(TwoWire *pWire,uint8_t mode)
+:DFRobot_ZXW(mode)
+{
+	_deviceADDR = DFRobot_Sensor_IIC_ADDR;
+	_pWire=pWire;
+}
+
+DFRobot_ZXW_SPI::DFRobot_ZXW_SPI(SPIClass *pSpi,uint8_t csPin=4,uint8_t mode)
+:DFRobot_ZXW(mode)
+{
+	_pSpi = pSpi;
+	_csPin = csPin;
+}
+
+int DFRobot_ZXW::begin(void)
+{
+	uint8_t id=0xDF;
+	uint8_t buf;
+	if(readReg(SENSOR_ADDR_ID,&buf,1)==0)
+	{
+		DBG("bus data access error");
+		return ERR_DATA_BUS;
+	}
+	DBG("real sensor id=");
+	DBG(id);
+	if(id != DFRobot_Sensor_ID)
+	{
+		return ERR_IC_VERSION;
+	}
+	write(SENSOR_ADDR_CONFIG,&_mode,1);
+	DBG("begin ok!");
+	return ERR_OK;
+}
+
+int DFRobot_ZXW_IIC::begin(void)
+{
+	Wire.begin();
+	return DFRobot_ZXW::begin();
+}
+
+int DFRobot_ZXW_SPI::begin(void)
+{
+	pinMode(_csPin,OUTPUT);//==========================
+	_pSpi->begin();
+	return DFRobot_ZXW::begin();
+}
+
+void DFRobot_ZXW_IIC::writeReg(uint8_t reg,void* pBuf,size_t size)
+{
+	if(pBuf == NULL)
+	{
+		DBG("pBuf ERROR = NULL");
+	}
+	uint8_t* _pBuf = (uint8_t*)pBuf;
+	_pWire->beginTransmission(_deviceADDR);
+	for(uint16_t i=0;i<size;i++)
+	{
+		_pWire->write(_pBuf[i]);
+	}
+	_pWire->endTransmission();
+}
+
+void DFRobot_ZXW_IIC::readReg(uint8_t reg,void* pBuf,size_t size)
+{
+	if(pBuf == BULL)
+	{
+		DBG("pBuf ERROR = NULL");
+	}
+	uint8_t * _pBuf = (uint8_t*)pBuf;
+	_pWire->beginTransmission(_deviceADDR);//使用指定的地址开始向I2C从设备进行传输。
+	if(_pWire->endTransmission()!=0)//用到了 Wire.endTransmission()，结束一个由beginTransmission（）
+									//开始的并且由write（）排列的从机的传输。返回0 成功 1 数据溢出 2 发送错误或其他错误
+	{
+		return 0;
+	}//控制器设备使用此函数从外围设备请求字节
+	_pWire->requestFrom(_deviceADDR,(uint8_t)size);//主设备请求从设备一个字节，这个字节可以被主设备用
+	for(uint16_t i=0;i<size;i++)
+	{
+		_pBuf[i] = _pWire->read();
+	}
+	_pWire->endTransmission();
+	return size;
+}
+
+void DFRobot_ZXW_SPI::writeReg(uint8_t reg,voif* pBuf,size_t size)
+{
+	if(pBuf == NULL)
+	{
+		DBG("pBuf ERROR = NULL");
+	}
+	uint8_t* _pBuf = (uint8_t)pBuf;
+	//==========================================
+	_pSpi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE));
+	_pSpi->transfer(reg);
+	while(size--)
+	{
+		_pSpi->transfer(*_pBuf);
+		_pBuf ++;
+	}
+	digitalWrite(_csPin,1);
+	_pSpi->endTransaction();
+}
+
+void DFRobot_ZXW_SPI::readReg(uint8_t reg,void* pBuf,size_t size)
+{
+	if(pBuf == BULL)
+	{
+		DBG("pBuf ERROR = NULL");
+	}
+	size_t count = 0;
+	uint8_t* _pBuf = (uint8_t)pBuf;
+	//==========================================
+	_pSpi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	_pSpi->transfer(reg);
+	while(size--)
+	{
+		*_pBuf = SPI.transfer(0x00);
+		_pBuf ++;
+		count++;
+	}
+	digitalWrite(_csPin,1);
+	_pSpi->endTransaction();
+	return count;
+}
+
+uint16_t DFRobot_ZXW::soundStrengthDB(void)
+{
+	sData_t data;
+	readReg(SENSOR_ADDR_DATA,&data,sizeof(data));
+	DBG("sound is");
+	DBG(data.sound);
+	return data.sound<<3;
+}
+
+uint16_t DFRobot_ZXW::lightStrengthLux(void)
+{
+	sData_t data;
+	readReg(SENSOR_ADDR_DATA,&data,sizeof(data));
+	DBG("light is");
+	DBG(data.light);
+	return data.light*10000;
+}
+
+uint16_t DFRobot_ZXW::switchMode(uint8_t mode)
+{
+	uint8_t temp;
+	#ifdef ENABLE_DBG
+	readReg(SENSOR_ADDR_CONFIG,&temp,sizeof(temp));
+	DBG("before switch mode = ");
+	DBG(mode);
+	#endif
+	writeReg(SENSOR_ADDR_CONFIG,&mode,sizeof(mode));
+	#ifdef ENABLE_DBG
+	readReg(SENSOR_ADDR_CONFIG,&temp,sizeof(temp));
+	DBG("after switch mode = ");
+	#endif
+}
+
+void DFRobot_ZXW::setLED(uint8_t r,uint8_t g,uint8_t b)
+{
+	sColor_t data={.b=b>>3,.g=g>>2,.r=r>>3};
+	writeReg(SENSOR_ADDR_LED,&data,sizeof(data));
+}
+
+void DFRobot_ZXW::setLED(uint16_t color)
+{
+	
+	writeReg(SENSOR_ADDR_LED,&color,1);
+}
--- DFRobot_ZXW.h
+++ DFRobot_ZXW.h
@@ -1,39 +1,153 @@
 /*!
-*	@file DFRobot_ZXW.h
-*	@brief 这是一个抽象传感器，可以设置人的姓名和年龄；并且可以判断你输入的名字和姓名是否规范；
-*	并且可以将姓名和年龄打印出来
-*	 @n 该类重写了构造函数和析构函数的方法，创建局部变量，并全打印出来；该类设置年龄也使用了重载，使用重载
-* 	的方法，能使输入的年龄可以是整数类型也可以是字符串类型。
-* 	@copyright Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
-* 	@licence The MIT License (MIT)
-* 	@author [Sivan](<A href="mailto:TZ@dfrobot.com">TZ@dfrobot.com</A>)
-* 	@version V1.0
-* 	@eGDAte 2019-07-28
-* 	@get from https://www.dfrobot.com
-* 	@url https://github.com/Sivan6/sivan
-*/
-
+ * @file DFRobot_Sensor.h
+ * @brief 定义DFRobot_Sensor 类的基础结构
+ * @n 这是一个虚拟的传感器，IIC地址不可改变,不对应任何实物，可以通过IIC和SPI口来控制它，假设它有下面这些功能
+ * @n 向寄存器0里写入数据，点亮不同颜色的LED灯
+ * @n 从寄存器1里读出数据，高四位表示光线强度，低四位表示声音强度
+ * @n 从寄存器2 bit0里写入数据，写1表示正常模式，写0表示低功耗模式
+ * @n 从寄存器3 读取数据，读到的是芯片版本0xDF
+ * @copyright	Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @licence     The MIT License (MIT)
+ * @author [Ouki](ouki.wang@dfrobot.com)
+ * @version  V1.0
+ * @date  2019-07-13
+ * @https://github.com/ouki-wang/DFRobot_Sensor
+ */
 
 #ifndef __DFROBOT_ZXW_H_
 #define __DFROBOT_ZXW_H_
 
 #include "Arduino.h"
+#include <Wire.h>
+#include <SPI.h>
+
+#define ENABLE_DBG //========================================
+#define DBG(...) {Serial.print(__FUNCTION__);Serial.print("()");Serial.print(__LINE__);Serial.println(__VA_ARGS__);}
+
+#define DFRobot_Sensor_IIC_ADDR 0x66 //芯片IIC地址，无变化地址功能
+#define DFRobot_Sensor_ID 0xDF //芯片IIC地址，无变化地址功能
+
+#define SENSOR_ADDR_LED    0 //LED控制地址  这里的描述从芯片手册上抄写
+#define SENSOR_ADDR_DATA   2 //组合数据地址  这里的描述从芯片手册上抄写
+#define SENSOR_ADDR_CONFIG 3 //配置寄存器地址 这里的描述从芯片手册上抄写
+#define SENSOR_ADDR_ID     4 //芯片ID寄存器地址 这里的描述从芯片手册上抄写
+
+#define  COLOR_RGB565_BLACK     0x0000      // 黑色    
+#define  COLOR_RGB565_NAVY      0x000F      // 深蓝色  
+#define  COLOR_RGB565_DGREEN    0x03E0      // 深绿色  
+#define  COLOR_RGB565_DCYAN     0x03EF      // 深青色  
+#define  COLOR_RGB565_PURPLE    0x780F      // 紫色  
+#define  COLOR_RGB565_MAROON    0x7800      // 深红色      
+#define  COLOR_RGB565_OLIVE     0x7BE0      // 橄榄绿      
+#define  COLOR_RGB565_LGRAY     0xC618      // 灰白色
+#define  COLOR_RGB565_DGRAY     0x7BEF      // 深灰色      
+#define  COLOR_RGB565_BLUE      0x001F      // 蓝色    
+#define  COLOR_RGB565_GREEN     0x07E0      // 绿色          
+#define  COLOR_RGB565_CYAN      0x07FF      // 青色  
+#define  COLOR_RGB565_RED       0xF800      // 红色       
+#define  COLOR_RGB565_MAGENTA   0xF81F      // 品红    
+#define  COLOR_RGB565_YELLOW    0xFFE0      // 黄色        
+#define  COLOR_RGB565_WHITE     0xFFFF      // 白色  
+
 class DFRobot_ZXW
 {
 public:
-	char name[20];
-	DFRobot_ZXW();		//构造函数；可以对其重写
-	~DFRobot_ZXW();		//析构函数；在程序销毁是执行
-	int setName(const char *name);	//赋值姓名函数；0成功，-1失败
-	int setAge(uint8_t age);		//赋值年龄函数；0成功，-1失败
-	int setAge(String age);			//赋值年龄函数；字符串类型赋值
-	String getName(void);			//取值函数；取到赋值姓名的函数
-	uint8_t getAge(void);			//取值函数；取到赋值年龄的函数
+	#define ERR_OK 0
+	#define ERR_DATA_BUS -1
+	#define ERR_IC_VERSION -2
 	
-private:	//私有化定义变量
-	String _name;
-	uint8_t _age;
+	typedef struct
+	{
+		uint8_t r:5;
+		uint8_t g:6;
+		uint8_t b:5;	
+	}__attribute__((packed)) sColor_t;
+	
+	typedef struct
+	{
+		uint8_t light:4;
+		uint8_t sound:4;
+	}__attribute__((packed)) sData_t;
+	 /*
+   这里从数据手册上抄写关于这个寄存器的描述
+     * -----------------------------------------------------------------------------------------
+     * |    b7    |    b6    |    b5    |    b4    |    b3    |    b2   |    b1     |    b0    |
+     * -----------------------------------------------------------------------------------------
+     * |   ready  |         reversed               |      precision     | highspeed | lowpower |
+     * -----------------------------------------------------------------------------------------
+     *
+     *上电后，ready位默认为1，不可更改
+  */
+  typedef struct {
+    uint8_t   lowpower: 1; /*!< 上电为0，1：低功耗模式 0：正常功耗模式 */
+    uint8_t   highspeed: 1; /*!< 上电为0，1：告诉模式 0：正常速度模式 */
+    uint8_t   precision: 2; /*!< 上电为0，0：低精度模式，1：正常精度模式，2：高精度模式，3：超高精度模式 */
+    uint8_t   reserved: 3; /*!< 上电为0，1：低功耗模式 0：正常功耗模式 */
+    uint8_t   ready:1; /*!< 上电为0，1：低功耗模式 0：正常功耗模式 */
+  } __attribute__ ((packed)) sMode_t;
+  
+	typedef enum
+	{
+		eNormalPower = 0,
+		eLowPower = 1,	
+	}ePowerMode_t;
+	
+	typedef enum
+	{
+		eNormalSpeed = 0<<1,
+		eHighSpeed = 1<<1,
+	}eSpeedMode_t;
+	
+	typedef enum
+	{
+		eLowPrecision = 0<<2,
+		eNormalPrecision = 1<<2,
+		eHighPrecision = 2<<2,
+		eUltraPrecision = 3<<2,
+	}ePrecisionMode_t;
+	
+public:
+	DFRobot_ZXW(uint8_t mode);
+	virtual int begin(void)=0;//=====
+	uint16_t soundStrengthDB(void);
+	uint16_t lightStrengthLux(void);
+	uint16_t switchMode(uint8_t mode);
+	void setLED(uint8_t r,uint8_t g,uint8_t b);
+	void setLED(uint16_t color);
+protected:
+	virtual uint8_t readReg(uint8_t reg,void* pBuf,size_t size)=0;
+	virtual void writeReg(uint8_t reg,void* pBuf,size_t size)=0;
+private:
+	uint8_t _r,_g,_b;
+	uint8_t _mode;
 };
+
+class DFRobot_ZXW_IIC:public DFRobot_ZXW
+{
+public:
+	DFRobot_ZXW_IIC(TwoWire *pWire=&Wire,uint8_t mode=eNormalPrecision+eNormalSpeed+eNormalPower);
+	virtual int begin(void);
+protected:
+	virtual uint8_t readReg(uint8_t reg,void* pBuf,size_t size);
+	virtual void writeReg(uint8_t reg,void* pBuf,size_t size);
+private:
+	TwoWire *_pWire;
+	uint8_t _deviceADDR;
+};
+
+class DFRobot_ZXW_SPI:public DFRobot_ZXW
+{
+public:
+	DFRobot_ZXW_SPI(SPIClass *pSpi=&SPI,uint8_t csPin=4,uint8_t mode=eNormalPrecision+eNormalSpeed+eNormalPower);
+	virtual int begin(void);
+protected:
+	virtual uint8_t readReg(uint8_t reg,void* pBuf,size_t size);
+	virtual void writeReg(uint8_t reg,void* pBuf,size_t size);
+private:
+	SPIClass *_pSpi;
+	uint8_t _csPin;
+};
+
 
 
 #endif
